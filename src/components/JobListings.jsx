ad9b201a02d51dd3348e691b956540f18cad6105
Edet Effiong <greatstory247@gmail.com>
2024-06-18 22:58:39 +0100

    UPDATE: joblislistings page error fixed

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,67 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import JobListing from './JobListing';
-// // import Spinner from './Spinner';
-
-
-
-// const JobListings = ({ isHome = false }) => {
-//   const [jobs, setJobs] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchJobs = async () => {
-//       try {
-//         const res = await fetch("http://localhost:8000"); // Correct URL
-//           if (!res.ok) {
-//             throw new Error('Network response was not ok');
-//           }
-//         const data = await res.json(); // Convert response to JSON
-//         setJobs(data);
-//       } catch (error) {
-//         console.log("Error fetching data:", error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchJobs();
-//   }, []);
-
-
-//   return (
-//     <section className='bg-blue-50 px-4 py-10'>
-//       <div className='container-xl lg:container m-auto'>
-//         <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-//           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
-//         </h2>
-//           {/* Conditionally render based on loading state  */}
-//           {/* {loading ? (
-//             <Spinner loading={loading}/>
-//           ) : ( */}
-//           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-//            .map((job) => (
-//               <JobListing key={job.id} job={job} />
-//             ))}
-//         </div>
-
-
-//        {/* )} */}
-//       </div>
-//     </section>
-//   );
-// };
-// export default JobListings;
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import JobListing from './JobListing';
 import Spinner from './Spinner'; 
@@ -70,10 +6,11 @@ const JobListings = ({ isHome = false, job }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
+
 useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await fetch("http://localhost:8000"); // Correct URLj
+        const res = await fetch("http://localhost:8000");
         if (!res.ok) {
           throw new Error('Network response was not ok');
         }
