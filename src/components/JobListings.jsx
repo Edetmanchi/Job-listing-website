17251f0ab3d55a74fc0c2895a9b336b08de1c2d7
Edet Effiong <greatstory247@gmail.com>
2024-06-21 23:30:20 +0100

    unit test

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -632,19 +632,4 @@ useEffect(() => {
         <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        {/* Conditionally render based on loading state */}
-        {loading ? (
-          <div className="text-center my-8">
-            <Spinner/>
-          </div>
-        ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  );
-};
+        {/* Conditionally 
