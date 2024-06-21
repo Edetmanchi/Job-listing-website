405d2accc3ab8452ccee3423e823b68d55980883
Edet Effiong <greatstory247@gmail.com>
2024-06-21 23:31:02 +0100

    bug fix

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -611,25 +611,4 @@ useEffect(() => {
           throw new Error('Network response was not ok');
         }
         console.log(res)
-        const data = await res.json();
-        setJobs(data);
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchJobs();
-  }, []);
-
-
-
-
-  return (
-    <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
-        </h2>
-        {/* Conditionally 
+        cons
